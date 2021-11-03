993e3448c7043a69ae98a2ad8e24bca6ef0aeba0
DennisTarik <dm499555@gmail.com>
2021-11-03 12:02:49 +0100

    :sparkles: add css and html

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,37 @@
 import React from 'react';
+import styles from './App.module.css';
 
 function App(): JSX.Element {
-  return <main>Hello Fishes!</main>;
+  return (
+    <main className={styles.container}>
+      <img
+        className={styles.image}
+        src="src/app/assets/people-partying-close-up-photography-2705089.jpg"
+        alt="Some people chilling on a table"
+      />
+      <div>
+        <h1 className={styles.title}>Bergfest Party Time </h1>
+        <form className={styles.form}>
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="First name"
+          />
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="First name"
+          />
+          <input
+            className={styles.submitbutton}
+            type="submit"
+            value="Sign in for Partey"
+            placeholder="Enter song here!"
+          />
+        </form>
+      </div>
+    </main>
+  );
 }
+
 export default App;
